605d3ba1c86e4be880a271c6ec14a78ae4723814
josephonwuchekw <josephonwu0@gmail.com>
2023-08-29 12:10:21 -0400

    ui layout

--- DataTableWidget.py
+++ DataTableWidget.py
@@ -15,49 +15,11 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(932, 567)
-        self.gridLayout = QtWidgets.QGridLayout(Form)
-        self.gridLayout.setContentsMargins(0, -1, 0, -1)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName("gridLayout")
-        self.tableColumn_Match = QtWidgets.QLabel(Form)
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.tableColumn_Match.setFont(font)
-        self.tableColumn_Match.setStyleSheet("#tableColumn_Match{\n"
-"    border:1px solid #000;\n"
-"    padding:10px;\n"
-"}")
-        self.tableColumn_Match.setAlignment(QtCore.Qt.AlignCenter)
-        self.tableColumn_Match.setObjectName("tableColumn_Match")
-        self.gridLayout.addWidget(self.tableColumn_Match, 0, 1, 1, 1)
-        self.tableColumn_Time = QtWidgets.QLabel(Form)
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.tableColumn_Time.setFont(font)
-        self.tableColumn_Time.setStyleSheet("#tableColumn_Time{\n"
-"    border:1px solid #000;\n"
-"    padding:10px;\n"
-"}")
-        self.tableColumn_Time.setAlignment(QtCore.Qt.AlignCenter)
-        self.tableColumn_Time.setObjectName("tableColumn_Time")
-        self.gridLayout.addWidget(self.tableColumn_Time, 0, 2, 1, 1)
-        self.tableColumn_No = QtWidgets.QLabel(Form)
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.tableColumn_No.setFont(font)
-        self.tableColumn_No.setStyleSheet("#tableColumn_No{\n"
-"    border:1px solid #000;\n"
-"    padding:10px;\n"
-"}")
-        self.tableColumn_No.setAlignment(QtCore.Qt.AlignCenter)
-        self.tableColumn_No.setObjectName("tableColumn_No")
-        self.gridLayout.addWidget(self.tableColumn_No, 0, 0, 1, 1)
         self.tableColumn_Platform = QtWidgets.QLabel(Form)
         font = QtGui.QFont()
         font.setPointSize(12)
@@ -71,6 +33,45 @@ class Ui_Form(object):
         self.tableColumn_Platform.setAlignment(QtCore.Qt.AlignCenter)
         self.tableColumn_Platform.setObjectName("tableColumn_Platform")
         self.gridLayout.addWidget(self.tableColumn_Platform, 0, 3, 1, 1)
+        self.tableColumn_Match = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.tableColumn_Match.setFont(font)
+        self.tableColumn_Match.setStyleSheet("#tableColumn_Match{\n"
+"    border:1px solid #000;\n"
+"    padding:10px;\n"
+"}")
+        self.tableColumn_Match.setAlignment(QtCore.Qt.AlignCenter)
+        self.tableColumn_Match.setObjectName("tableColumn_Match")
+        self.gridLayout.addWidget(self.tableColumn_Match, 0, 1, 1, 1)
+        self.tableColumn_No = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.tableColumn_No.setFont(font)
+        self.tableColumn_No.setStyleSheet("#tableColumn_No{\n"
+"    border:1px solid #000;\n"
+"    padding:10px;\n"
+"}")
+        self.tableColumn_No.setAlignment(QtCore.Qt.AlignCenter)
+        self.tableColumn_No.setObjectName("tableColumn_No")
+        self.gridLayout.addWidget(self.tableColumn_No, 0, 0, 1, 1)
+        self.tableColumn_Time = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.tableColumn_Time.setFont(font)
+        self.tableColumn_Time.setStyleSheet("#tableColumn_Time{\n"
+"    border:1px solid #000;\n"
+"    padding:10px;\n"
+"}")
+        self.tableColumn_Time.setAlignment(QtCore.Qt.AlignCenter)
+        self.tableColumn_Time.setObjectName("tableColumn_Time")
+        self.gridLayout.addWidget(self.tableColumn_Time, 0, 2, 1, 1)
         self.tableColumn_Tips = QtWidgets.QLabel(Form)
         font = QtGui.QFont()
         font.setPointSize(12)
@@ -84,21 +85,18 @@ class Ui_Form(object):
         self.tableColumn_Tips.setAlignment(QtCore.Qt.AlignCenter)
         self.tableColumn_Tips.setObjectName("tableColumn_Tips")
         self.gridLayout.addWidget(self.tableColumn_Tips, 0, 4, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem, 2, 2, 1, 1)
-        self.dataRecordsLayout = QtWidgets.QVBoxLayout()
-        self.dataRecordsLayout.setContentsMargins(-1, 20, -1, 20)
-        self.dataRecordsLayout.setObjectName("dataRecordsLayout")
-        self.gridLayout.addLayout(self.dataRecordsLayout, 1, 0, 1, 5)
+        self.verticalLayout.addItem(spacerItem)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.tableColumn_Match.setText(_translate("Form", "Match"))
-        self.tableColumn_Time.setText(_translate("Form", "Time"))
-        self.tableColumn_No.setText(_translate("Form", "No."))
+        Form.setWindowTitle(_translate("Form", "Sports Scraper"))
         self.tableColumn_Platform.setText(_translate("Form", "Platform"))
+        self.tableColumn_Match.setText(_translate("Form", "Match"))
+        self.tableColumn_No.setText(_translate("Form", "No."))
+        self.tableColumn_Time.setText(_translate("Form", "Time"))
         self.tableColumn_Tips.setText(_translate("Form", "Tips"))
--- main.py
+++ main.py
@@ -13,7 +13,134 @@ class MainWindow(qtw.QWidget):
         self.ui = DataTableUI()
         self.ui.setupUi(self)
 
+        dataModel = [
+            {
+                "id": 1,
+                "platform": {
+                    "name": "platform 1",
+                    "url": "www.platform.com",
+                    "matches": [
+                        {
+                            "home": "arsenal",
+                            "away": "manchester united",
+                            "kickoff": "12:00",
+                            "tip": "1 - 2",
+                            "platform": "platform 1"
+                        },
+                        {
+                            "home": "liverpool",
+                            "away": "manchester city",
+                            "kickoff": "12:00",
+                            "tip": "2 - 3",
+                            "platform": "platform 1"
+                        }
+                    ],
+                },
+            },
+            {
+                "id": 2,
+                "platform": {
+                    "name": "platform 2",
+                    "url": "www.platform.com",
+                    "matches": [
+                        {
+                            "home": "arsenal",
+                            "away": "manchester united",
+                            "kickoff": "12:00",
+                            "tip": "1 - 1",
+                            "platform": "platform 2"
+                        },
+                        {
+                            "home": "liverpool",
+                            "away": "manchester city",
+                            "kickoff": "12:00",
+                            "tip": "1 - 3",
+                            "platform": "platform 2"
+                        }
+                    ],
+                },
+            }
+        ]
+
+        self.populateRows(dataModel)
         self.show()
+
+    def populateRows(self, data):
+        allMatches = []
+        platforms = []
+        cellFont = qtg.QFont()
+        cellFont.setPointSize(12)
+
+        for entry in data:
+            matchList = entry["platform"]["matches"]
+            allMatches += matchList
+
+        for i in range(len(allMatches)):
+            # Index
+            idxObjName = f"indexField_{i}"
+            indexField = qtw.QLabel()
+            indexField.setText(str(i+1))
+            indexField.setFont(cellFont)
+            indexField.setAlignment(qtc.Qt.AlignCenter)
+            indexField.setObjectName(idxObjName)
+            indexField.setStyleSheet("#"+idxObjName+"{\n"
+                                     "    border:1px solid #ccc;\n"
+                                     "    padding:10px;\n"
+                                     "}")
+            self.ui.gridLayout.addWidget(indexField, i+1, 0, 1, 1)
+
+            # Match
+            matchObjName = f"matchField_{i}"
+            matchField = qtw.QLabel()
+            matchField.setText(
+                f'{allMatches[i]["home"].capitalize()} - {allMatches[i]["away"].capitalize()}')
+            matchField.setFont(cellFont)
+            matchField.setAlignment(qtc.Qt.AlignCenter)
+            matchField.setObjectName(matchObjName)
+            matchField.setStyleSheet("#"+matchObjName+"{\n"
+                                     "    border:1px solid #ccc;\n"
+                                     "    padding:10px;\n"
+                                     "}")
+            self.ui.gridLayout.addWidget(matchField, i+1, 1, 1, 1)
+
+            # Time
+            timeObjName = f"timeField_{i}"
+            timeField = qtw.QLabel()
+            timeField.setText(f'{allMatches[i]["kickoff"]}')
+            timeField.setFont(cellFont)
+            timeField.setAlignment(qtc.Qt.AlignCenter)
+            timeField.setObjectName(timeObjName)
+            timeField.setStyleSheet("#"+timeObjName+"{\n"
+                                    "    border:1px solid #ccc;\n"
+                                    "    padding:10px;\n"
+                                    "}")
+            self.ui.gridLayout.addWidget(timeField, i+1, 2, 1, 1)
+
+            # Platform
+            platformObjName = f"platformField_{i}"
+            platformField = qtw.QLabel()
+            platformField.setText(f'{allMatches[i]["platform"]}')
+            platformField.setFont(cellFont)
+            platformField.setAlignment(qtc.Qt.AlignCenter)
+            platformField.setObjectName(platformObjName)
+            platformField.setStyleSheet("#"+platformObjName+"{\n"
+                                        "    border:1px solid #ccc;\n"
+                                        "    padding:10px;\n"
+                                        "}")
+            self.ui.gridLayout.addWidget(platformField, i+1, 3, 1, 1)
+
+            # Tips
+            tipsObjName = f"tipsField_{i}"
+            tipsField = qtw.QLabel()
+            tipsField.setText(f'{allMatches[i]["tip"]}')
+            tipsField.setFont(cellFont)
+            tipsField.setAlignment(qtc.Qt.AlignCenter)
+            tipsField.setObjectName(tipsObjName)
+            tipsField.setStyleSheet("#"+tipsObjName+"{\n"
+                                    "    border:1px solid #ccc;\n"
+                                    "    padding:10px;\n"
+                                    "}")
+            self.ui.gridLayout.addWidget(tipsField, i+1, 4, 1, 1)
 
 
 if __name__ == '__main__':
